04c458169c53615f84d05b4f164707146cba03a7
TakumiOkayasu <okayasutakumi26@gmail.com>
2024-11-19 00:49:41 +0900

    fix: src code position

--- profile.ps1
+++ profile.ps1
@@ -1,7 +1,23 @@
-$script = @("$Home\psrc\aliases.ps1", "$Home\psrc\function.ps1", "$Home\psrc\prompt.ps1")
+$script = @("$PWD\aliases.ps1", "$PWD\function.ps1", "$PWD\prompt.ps1")
+
+# プロファイルディレクトリの確認
+$PROFILE | Split-Path -Parent
+
+# ディレクトリが存在しない場合は作成
+if (!(Test-Path -Path ($PROFILE | Split-Path -Parent))) {
+  New-Item -ItemType Directory -Path ($PROFILE | Split-Path -Parent) -Force
+}
+
+# シンボリックリンクが存在しない場合のみ作成
+if (!(Test-Path $PROFILE)) {
+  $profileName = Split-Path $PROFILE -Leaf
+  New-Item -ItemType SymbolicLink -Path $PROFILE -Target (Join-Path $PWD $profileName) -Force
+}
 
 foreach ($s in $script) {
   if (Test-Path $s) {
     . $s
   }
 }
+
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
